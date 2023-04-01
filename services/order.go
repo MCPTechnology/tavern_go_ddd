9d569593ce42257f97dd3b7eab85e0329cd24390
Matheus Caputo Pires <matheus.piresufv@gmail.com>
2023-03-31 21:37:47 -0300

    Adding Order Service with Customer Repository

--- services/order.go
+++ services/order.go
@@ -2,7 +2,9 @@
 package services
 
 import (
+	"github.com/google/uuid"
 	"github.com/matheuscaputopires/ddd-go/domain/customer"
+	"github.com/matheuscaputopires/ddd-go/domain/customer/memory"
 )
 
 // OrderConfiguration is an alias for a function that will take in a pointer to an OrderService and modify it
@@ -27,4 +29,31 @@ func NewOrderService(cfgs ...OrderConfiguration) (*OrderService, error) {
 		}
 	}
 	return os, nil
+}
+
+// WithCustomerRepository applies a given customer repository to the OrderService
+func WithCustomerRepository(cr customer.CustomerRepository) OrderConfiguration {
+	// return a function that matches the OrderConfiguration alias,
+	// This is done so that the parent function can take in all the needed parameters
+	return func (os *OrderService) error {
+		os.customer = cr
+		return nil
+	}
+}
+
+// WithMemoryCustomerRepository applies a memory customer repository to the OrderService
+func WithMemoryCustomerRepository() OrderConfiguration {
+	// Create the memory repo, if paramaeters such as connection strings, they could be input here
+	cr := memory.New()
+	return WithCustomerRepository(cr)
+}
+
+func (o *OrderService) CreateOrder(customerID uuid.UUID, productIDs []uuid.UUID) error {
+	// Get the customer
+	c, err := o.customer.Get(customerID)
+	if err != nil {
+		return err
+	}
+	
+	return nil
 }
